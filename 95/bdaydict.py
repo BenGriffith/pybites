683a9bf2b5bf116f96721fc5af4e8410e3264f56
Ben Griffith <bengriffith@outlook.com>
2021-08-28 13:57:55 -0500

    submission Bite 95 @ codechalleng.es

--- 95/bdaydict.py
+++ 95/bdaydict.py
@@ -6,4 +6,17 @@ class BirthdayDict(dict):
        the same birthday (day+month) as somebody already in the dict"""
 
     def __setitem__(self, name, birthday):
-        pass
+        
+        for key, value in dict.items(self):
+            if birthday.month == value.month and birthday.day == value.day:
+                print(MSG.format(name))
+
+        dict.__setitem__(self, name, birthday)    
+
+
+# bd = BirthdayDict()
+# bd['bob'] = date(1987, 6, 15)
+# bd['tim'] = date(1984, 7, 15)
+# bd['mary'] = date(1987, 6, 15)
+# bd['sara'] = date(1987, 6, 14)
+# bd['mike'] = date(1981, 7, 15)
