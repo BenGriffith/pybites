0170a9f0390714d5833b8d0f7b847d9e10d32849
Ben Griffith <bengriffith@outlook.com>
2022-01-23 20:35:21 +0000

    submission Bite 85 @ codechalleng.es

--- 85/ninja.py
+++ 85/ninja.py
@@ -13,17 +13,20 @@ class NinjaBelt:
         self._last_earned_belt = None
 
     def _get_belt(self, new_score):
-        """Might be a useful helper"""
         for belt_score, belt_color in BELTS.items():
-            if belt_score > self._last_earned_belt:
-                self._last_earned_belt = belt_color.capitalize()
+            if new_score >= belt_score:
+                self._last_earned_belt = belt_color
+        return self._last_earned_belt
         
     def _get_score(self):
         return self._score
 
     def _set_score(self, new_score):
-        if not isinstance(new_score, int) or new_score < self._get_score():
-            raise ValueError
+        if not isinstance(new_score, int):
+            raise ValueError("Score takes an int")
+
+        if new_score < self._get_score():
+            raise ValueError("Cannot lower score")
 
         self._score = new_score        
         current_belt = self._last_earned_belt
@@ -31,7 +34,12 @@ class NinjaBelt:
         if new_belt == current_belt:
             print(f"Set new score to {self._score}")
         else:
-            print(f"Congrats you earned {new_score} points obtaining the PyBites Ninja {new_belt} Belt")
-
+            print(f"Congrats, you earned {new_score} points obtaining the PyBites Ninja {new_belt.capitalize()} Belt")
 
     score = property(_get_score, _set_score)
+
+
+if __name__ == "__main__":
+    ninja = NinjaBelt()
+    ninja.score = 20
+
