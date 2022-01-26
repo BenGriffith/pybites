34bfaf23b8a51de7266a97623a773294682e359c
Ben Griffith <bengriffith@outlook.com>
2022-01-26 13:42:37 +0000

    submission Bite 65 @ codechalleng.es

--- 65/scrabble.py
+++ 65/scrabble.py
@@ -1,3 +1,4 @@
+from audioop import reverse
 import itertools
 import os
 import urllib.request
@@ -20,32 +21,35 @@ def get_possible_dict_words(draw):
        valid dictionary words. Use _get_permutations_draw and provided
        dictionary"""
     possible_words = []
+
     list_of_permutations = _get_permutations_draw(draw)
+
     for word in list_of_permutations:
+
         word = word.lower()
         if word in dictionary:
             possible_words.append(word)
     
     return possible_words
 
+
 def _get_permutations_draw(draw):
     """Helper to get all permutations of a draw (list of letters), hint:
        use itertools.permutations (order of letters matters)"""
     words = []
 
-    #draw = draw.replace(", ", "")
-    for i in range(len(draw) +1):
-        #print(draw[i])
-        temp = list(draw[:i +1])
-        print(temp[:i])
-        for j in itertools.permutations(temp, i):
-            print(j)
-            #words.append("".join(j))
+    for i in range(1, len(draw) + 1):
+        
+        list_of_permutations = list(itertools.permutations(draw, i))
+
+        for word in list_of_permutations:
+            words.append("".join(list(word)))
+        
     return words
 
 
-if __name__ == "__main__":
-    letters = 'T, I, I, G, T, T, L'.split(", ")
-    #letters1 = 'B, R, C, O, O, E'.split(",")
-    #print(_get_permutations_draw(letters))
-    print(get_possible_dict_words(letters))
+# if __name__ == "__main__":
+#     letters  = 'T, I, I, G, T, T, L'.split(", ")
+#     letters1 = 'B, R, C, O, O, E, O'.split(", ")
+#     letters2 = 'O, N, V, R, A, Z, H'.split(", ")
+#     print(get_possible_dict_words(letters2))
