c07cd05c95ba75a36a8b42743ea6ea445b65390c
Ben Griffith <bengriffith@outlook.com>
2022-02-10 13:38:11 +0000

    initial commit

---
+++ 258/flux.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+XYZ = "https://bites-data.s3.us-east-2.amazonaws.com/xyz.csv"
+THRESHOLDS = (5000, 0.05)
+
+
+def calculate_flux(XYZ: str) -> list:
+    """Read the data in from xyz.csv
+    add two new columns, one to calculate dollar flux,
+    and the other to calculate percentage flux
+    return as a list of tuples
+    """
+    df = pd.read_csv(XYZ)
+    print(df.shape[0])
+    df = df.astype({"12/31/20": int, "12/31/19": int})
+    df["dollar_flux"] = abs(df["12/31/20"] - df["12/31/19"])
+    df["percentage_flux"] = abs(df["dollar_flux"] / df["12/31/19"])
+
+    flux = [*zip(df["dollar_flux"], df["percentage_flux"])]
+
+    return flux
+
+
+def identify_flux(xyz: list) -> list:
+    """Load the list of tuples, iterate through
+    each item and determine if it is above both
+    thresholds. if so, add to the list
+    """
+    flagged_lines = []
+
+    return flagged_lines
+
+
+if __name__ == "__main__":
+    print(calculate_flux(XYZ))
