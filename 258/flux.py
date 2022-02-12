9234a8a28885a3b687a6fb5d420c893009711a41
Ben Griffith <bengriffith@outlook.com>
2022-02-12 02:34:03 +0000

    submission Bite 258 @ codechalleng.es

--- 258/flux.py
+++ 258/flux.py
@@ -11,12 +11,13 @@ def calculate_flux(XYZ: str) -> list:
     return as a list of tuples
     """
     df = pd.read_csv(XYZ)
-    print(df.shape[0])
     df = df.astype({"12/31/20": int, "12/31/19": int})
     df["dollar_flux"] = abs(df["12/31/20"] - df["12/31/19"])
     df["percentage_flux"] = abs(df["dollar_flux"] / df["12/31/19"])
 
-    flux = [*zip(df["dollar_flux"], df["percentage_flux"])]
+    flux = []
+    for row in df.itertuples():
+        flux.append((row.Account, row._2, row._3, row.dollar_flux, row.percentage_flux))
 
     return flux
 
@@ -29,11 +30,11 @@ def identify_flux(xyz: list) -> list:
     flagged_lines = []
 
     for flux in xyz:
-        if flux[0] >= THRESHOLDS[0] and flux[1] >= THRESHOLDS[1]:
-            flagged_lines.append()
+        if flux[3] > THRESHOLDS[0] and flux[4] > THRESHOLDS[1]:
+            flagged_lines.append(flux)
 
     return flagged_lines
 
 
-if __name__ == "__main__":
-    print(calculate_flux(XYZ))
+# if __name__ == "__main__":
+#     print(identify_flux(calculate_flux(XYZ)))
