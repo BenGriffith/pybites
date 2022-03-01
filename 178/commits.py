7a12bc9bd2ffdd29db6edac8f3a9b6c2b4b4ef41
Ben Griffith <bengriffith@outlook.com>
2022-03-01 14:10:47 +0000

    updated row changes calculation and added Counter object

--- 178/commits.py
+++ 178/commits.py
@@ -28,17 +28,26 @@ def get_min_max_amount_of_commits(commit_log: str = commits,
     """
     with open(commit_log) as file:
         commit_data = file.readlines()
+        commit_changes = Counter()
 
-        for commit in commit_data[:1]:
+        for commit in commit_data:
             row_date, row_stats = commit.strip("\n").split("|")
             row_date = parse(row_date.lstrip("Date:").strip(), ignoretz=True)
+            row_date_string = row_date.strftime('%Y-%m')
 
             row_stats = row_stats.split(",")
-            row_stats_insertion = row_stats[1].strip()[0]
-            row_stats_deletion = row_stats[2].strip()[0]
+            if len(row_stats) == 3:
+                row_stats_changes = int(row_stats[1].strip()[0]) + int(row_stats[2].strip()[0])
+            else:
+                row_stats_changes = int(row_stats[1].strip()[0])
 
-    return row_date, row_stats_insertion, row_stats_deletion
+            if year:
+                if year == row_date.year:
+                    commit_changes.update({row_date_string: row_stats_changes})
+            else:
+                commit_changes.update({row_date_string: row_stats_changes})
 
+    print(commit_changes)
 
 if __name__ == "__main__":
-    print(get_min_max_amount_of_commits())
+    print(get_min_max_amount_of_commits(commits, 2018))
