d33cee7e99c06982a731fed8b267705a47ce76e5
Ben Griffith <bengriffith@outlook.com>
2022-03-03 13:25:33 +0000

    submission Bite 233 @ codechalleng.es

--- 233/files.py
+++ 233/files.py
@@ -3,15 +3,22 @@ import os
 from pathlib import Path, PosixPath
 from zipfile import ZipFile
 
-TMP = Path(os.getenv("TMP", "/com.docker.devenvironments.code/233/tmp"))
+TMP = Path(os.getenv("TMP", "/tmp"))
 LOG_DIR = TMP / 'logs'
 ZIP_FILE = 'logs.zip'
 
 
 def zip_last_n_files(directory: PosixPath = LOG_DIR, zip_file: str = ZIP_FILE, n: int = 3):
-    for file in directory.iterdir():
-        print(file)
+    for file_path in directory.iterdir():
+        file_creation_date = datetime.fromtimestamp(file_path.stat().st_mtime).strftime('%Y-%m-%d')
+        file_path_stem, file_path_suffix = file_path.stem, file_path.suffix
+        os.rename(file_path, f"{file_path.parent}/{file_path_stem}_{file_creation_date}{file_path_suffix}")
+
+    with ZipFile(zip_file, "w") as archive:
+        for i, file_path in enumerate(sorted(directory.iterdir(), reverse=True)):
+            if i < n:
+                archive.write(file_path, arcname=file_path.name)
 
 
-if __name__ == "__main__":
-    zip_last_n_files()
+# if __name__ == "__main__":
+#     zip_last_n_files()
