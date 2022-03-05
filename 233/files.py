339d74d9b45f9d3668926b4e5b6a3aa4037aff61
Ben Griffith <bengriffith@outlook.com>
2022-03-05 02:47:42 +0000

    submission Bite 233 @ codechalleng.es

--- 233/files.py
+++ 233/files.py
@@ -1,7 +1,9 @@
+from audioop import reverse
 from datetime import datetime
 import os
 from pathlib import Path, PosixPath
 from zipfile import ZipFile
+
 
 TMP = Path(os.getenv("TMP", "/tmp"))
 LOG_DIR = TMP / 'logs'
@@ -9,15 +11,17 @@ ZIP_FILE = 'logs.zip'
 
 
 def zip_last_n_files(directory: PosixPath = LOG_DIR, zip_file: str = ZIP_FILE, n: int = 3):
-    for file_path in directory.iterdir():
-        file_creation_date = datetime.fromtimestamp(file_path.stat().st_mtime).strftime('%Y-%m-%d')
-        file_path_stem, file_path_suffix = file_path.stem, file_path.suffix
-        os.rename(file_path, f"{file_path.parent}/{file_path_stem}_{file_creation_date}{file_path_suffix}")
-
+    
     with ZipFile(zip_file, "w") as archive:
-        for i, file_path in enumerate(sorted(directory.iterdir(), reverse=True)):
+        for i, file_path in enumerate(sorted(directory.iterdir(), key=lambda x: x.stat().st_mtime, reverse=True)):
+
             if i < n:
-                archive.write(file_path, arcname=file_path.name)
+                file_creation_date = datetime.fromtimestamp(file_path.stat().st_mtime).strftime('%Y-%m-%d')
+                file_renamed_name = f"{file_path.stem}_{file_creation_date}{file_path.suffix}"
+                file_renamed_path = f"{file_path.parent}/{file_renamed_name}"
+
+                os.rename(file_path, file_renamed_path)
+                archive.write(file_renamed_path, arcname=file_renamed_name)
 
 
 # if __name__ == "__main__":
