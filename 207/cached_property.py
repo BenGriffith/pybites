0c19036c7639508e7b00fdd1eb12815ec5502764
Ben Griffith <bengriffith@outlook.com>
2022-08-14 19:45:38 -0500

    submission Bite 207 @ codechalleng.es

--- 207/cached_property.py
+++ 207/cached_property.py
@@ -1,10 +1,22 @@
 from random import random
 from time import sleep
+from functools import wraps
 
 
 def cached_property(func):
     """decorator used to cache expensive object attribute lookup"""
-    ...
+    cache = {}
+
+    @property
+    @wraps(func)
+    def wrapper(*args):
+    
+        planet = args[0]
+        if planet not in cache:
+            cache[planet] = func(*args)
+
+        return cache[planet]
+    return wrapper
 
 
 class Planet:
@@ -21,14 +33,10 @@ class Planet:
     def __repr__(self):
         return f'{self.__class__.__name__}({repr(self.color)})'
 
-    @property
+    @cached_property
     def mass(self):
         scale_factor = random()
         sleep(self.TEMPORAL_SHIFT)
         self._mass = (f'{round(scale_factor * self.GRAVITY_CONSTANT, 4)} '
                       f'{self.SOLAR_MASS_UNITS}')
         return self._mass
-
-    @mass.setter
-    def mass(self, value):
-        self._mass = value
